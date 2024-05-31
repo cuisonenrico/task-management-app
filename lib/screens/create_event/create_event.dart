1027071bb38b701c9fac6a2bae0e86d44b999b6b
cuisonenrico <cuisonenrico@gmail.com>
2024-05-30 21:07:54 -0400

    Create Event UI, Models, Actions,
    - added a switch for [isAllDay]
    - made an extension for displaying time

--- lib/screens/create_event/create_event.dart
+++ lib/screens/create_event/create_event.dart
@@ -10,6 +10,8 @@ import 'package:task_management_app/screens/widgets/input_field.dart';
 import 'package:task_management_app/screens/widgets/primary_button.dart';
 import 'package:task_management_app/state/meeting_state/model/event.dart';
 import 'package:task_management_app/utilities/app_router.dart';
+import 'package:task_management_app/utilities/default_colors.dart';
+import 'package:task_management_app/utilities/extensions/date_time_extensions.dart';
 import 'package:task_management_app/utilities/extensions/svg_ext.dart';
 import 'package:task_management_app/utilities/styles/spacers.dart';
 import 'package:task_management_app/utilities/styles/styles.dart';
@@ -73,62 +75,84 @@ class CreateEvent extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const FloatingContainer(
-                    margin: EdgeInsets.symmetric(horizontal: defaultHalfPadding),
+                  FloatingContainer(
+                    margin: const EdgeInsets.symmetric(horizontal: defaultHalfPadding),
+                    padding: const EdgeInsets.symmetric(vertical: defaultHalfPadding),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        // TODO: implement radio button for [isAllDay]
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: defaultHalfPadding),
-                    child: AppExpansionTile(
-                      padding: EdgeInsets.zero,
-                      onExpansionChanged: (_) {},
-                      displayedText: event.from == null ? 'From' : event.from.toString(),
-                      displayedTextTextStyle: TextStyles.body1,
-                      icon: Assets.svgImages.chevronDown.svg,
-                      children: [
-                        SizedBox(
-                          height: _datePickerHeight,
-                          width: double.infinity,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: defaultQuarterPadding),
-                            child: CupertinoDatePicker(
-                              mode: CupertinoDatePickerMode.time,
-                              initialDateTime: DateTime.now(),
-                              // This is called when the user changes the dateTime.
-                              onDateTimeChanged: (fromTime) => onChangedEvent(event.copyWith(from: fromTime)),
-                            ),
-                          ),
+                        Switch(
+                          inactiveThumbColor: primaryColor,
+                          inactiveTrackColor: Colors.white,
+                          value: event.isAllDay,
+                          onChanged: (value) => onChangedEvent(event.copyWith(isAllDay: value)),
+                        ),
+                        const HorizontalSpace(defaultQuarterPadding),
+                        Text(
+                          'Is this an all day event?',
+                          style: TextStyles.label2.copyWith(fontSize: 16),
                         ),
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: defaultHalfPadding),
-                    child: AppExpansionTile(
-                      padding: EdgeInsets.zero,
-                      onExpansionChanged: (_) {},
-                      displayedText: event.to == null ? 'To' : event.to.toString(),
-                      displayedTextTextStyle: TextStyles.body1,
-                      icon: Assets.svgImages.chevronDown.svg,
-                      children: [
-                        SizedBox(
-                          height: _datePickerHeight,
-                          width: double.infinity,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: defaultQuarterPadding),
-                            child: CupertinoDatePicker(
-                              mode: CupertinoDatePickerMode.time,
-                              initialDateTime: DateTime.now(),
-                              // This is called when the user changes the dateTime.
-                              onDateTimeChanged: (toTime) => onChangedEvent(event.copyWith(to: toTime)),
+                  Visibility(
+                    visible: event.isAllDay == false,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: defaultHalfPadding),
+                      child: AppExpansionTile(
+                        padding: EdgeInsets.zero,
+                        onExpansionChanged: (_) {},
+                        displayedText: event.from.toTimeFormatted('From'),
+                        displayedTextTextStyle: TextStyles.body1,
+                        icon: Assets.svgImages.chevronDown.svg,
+                        children: [
+                          SizedBox(
+                            height: _datePickerHeight,
+                            width: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: defaultQuarterPadding),
+                              child: CupertinoDatePicker(
+                                mode: CupertinoDatePickerMode.time,
+                                initialDateTime: DateTime.now(),
+                                // This is called when the user changes the dateTime.
+                                onDateTimeChanged: (fromTime) => onChangedEvent(event.copyWith(from: fromTime)),
+                              ),
                             ),
                           ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  IgnorePointer(
+                    ignoring: event.from == null,
+                    child: Visibility(
+                      visible: event.isAllDay == false,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: defaultHalfPadding),
+                        child: AppExpansionTile(
+                          padding: EdgeInsets.zero,
+                          onExpansionChanged: (_) {},
+                          displayedText: event.to.toTimeFormatted('To'),
+                          displayedTextTextStyle: TextStyles.body1,
+                          icon: Assets.svgImages.chevronDown.svg,
+                          children: [
+                            SizedBox(
+                              height: _datePickerHeight,
+                              width: double.infinity,
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: defaultQuarterPadding),
+                                child: CupertinoDatePicker(
+                                  mode: CupertinoDatePickerMode.time,
+                                  initialDateTime: DateTime.now(),
+                                  minimumDate: event.from,
+                                  // This is called when the user changes the dateTime.
+                                  onDateTimeChanged: (toTime) => onChangedEvent(event.copyWith(to: toTime)),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 ],
@@ -136,9 +160,9 @@ class CreateEvent extends StatelessWidget {
               InputField(
                 hintText: 'What will you be doing at this time?',
                 maxLines: 5,
-                onChangeText: (_) {},
+                onChangeText: (description) => onChangedEvent(event.copyWith(description: description)),
               ),
-              const VerticalSpace(16.0),
+              const VerticalSpace(defaultHalfPadding),
               Padding(
                 padding: const EdgeInsets.all(defaultQuarterPadding),
                 child: Row(
